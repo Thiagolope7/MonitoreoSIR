f40e7e05e8df6ca3a898ffbe9abdbe9711ec58c1
SANTIAGO AGUDELO LOPERA <santiagoagudelo152188@correo.itm.edu.co>
2019-03-12 09:41:54 -0500

    Comienzo procesos

--- DatosTrafico/DatosTrafico.Designer.cs
+++ DatosTrafico/DatosTrafico.Designer.cs
@@ -37,6 +37,13 @@
             this.label6 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnStart = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -114,13 +121,103 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(45, 169);
+            this.btnStart.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnStart.FlatAppearance.BorderSize = 10;
+            this.btnStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStart.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStart.Location = new System.Drawing.Point(42, 145);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 6;
-            this.btnStart.Text = "button1";
+            this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkGray;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(293, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CCTV";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.DarkGray;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(293, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 19);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "CCTV";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.DarkGray;
+            this.label8.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(293, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 19);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "CCTV";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 10;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(332, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.LimeGreen;
+            this.label10.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(403, 24);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 16);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.LimeGreen;
+            this.label11.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(406, 65);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(45, 16);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.LimeGreen;
+            this.label12.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(403, 105);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 16);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "label12";
             // 
             // DatosTrafico
             // 
@@ -128,6 +225,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
             this.ClientSize = new System.Drawing.Size(617, 332);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -153,5 +257,12 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
--- DatosTrafico/DatosTrafico.cs
+++ DatosTrafico/DatosTrafico.cs
@@ -6,6 +6,7 @@ namespace DatosTrafico
     using System.ComponentModel;
     using System.Data;
     using System.Data.SqlClient;
+    using System.Diagnostics;
     using System.Drawing;
     using System.IO;
     using System.Linq;
@@ -20,7 +21,8 @@ namespace DatosTrafico
         SqlConnection Conexion;
         public DatosTrafico()
         {
-            InitializeComponent();     
+            InitializeComponent();
+            ConexionSQL();
         }
 
         public void ConexionSQL()
@@ -32,19 +34,19 @@ namespace DatosTrafico
             catch (Exception ex)
             {
                 MessageBox.Show("No se conecto con la base de datos:" + ex.ToString());
-
             }
             return;
-
         }
 
         static void Mail(string Mensaje)
         {
+            DateTime TimeMail = DateTime.Now;
+            string TimeMail1 = TimeMail.ToString("yyyy-MM-dd HH:mm");
             var fromAddress = new MailAddress("diagindra@gmail.com", "Rutina Hermes");
             var toAddress = new MailAddress("santiagolopera13@gmail.com", "Santiago Agudelo");
             const string fromPassword = "Medellin2017a!";
             const string subject = "Alerta";
-            string body = Mensaje;
+            string body = Mensaje+" "+TimeMail1;
             MailAddress copyD = new MailAddress("davidmartinez.189@gmail.com", "David Martinez");
             MailAddress copyE = new MailAddress("elmer.aua@gmail.com", "Elmer Usuga");
             var smtp = new SmtpClient
@@ -68,10 +70,10 @@ namespace DatosTrafico
                     smtp.Send(message);
                     FileStream ArchivoTxT = new FileStream("C:/Traza/MAIL.txt", FileMode.Append, FileAccess.Write);
                     StreamWriter Escribir = new StreamWriter(ArchivoTxT);
-                    Escribir.Write(DateTime.Now+"Se envió mail correctamente con el mensaje"+Mensaje);
+                    Escribir.Write(DateTime.Now+" Se envió mail correctamente con el mensaje -> "+Mensaje);
                     Escribir.WriteLine();
                     Escribir.Flush();
-                    Escribir.Close();
+                    Escribir.Close();                 
                 }
                 catch (Exception ex)
                 {
@@ -81,24 +83,22 @@ namespace DatosTrafico
                     Escribir.WriteLine();
                     Escribir.Flush();
                     Escribir.Close();
-                }
+                }        
         }
-
 
         public void CCTV()
         {
-            ConexionSQL();
             Conexion.Open();
             DateTime Ahora = DateTime.Now;
             string AhoraString = Ahora.ToString("yyyy-MM-dd HH:mm:ss.FFF");
             DateTime Ahora1 = Ahora.AddMinutes(-30);
             string Ahora1String = Ahora1.ToString("yyyy-MM-dd HH:mm:ss.FFF");
+           
 
             SqlDataAdapter da = new SqlDataAdapter("select top 10 Logic_code,Fecha,Intensidad,VehiculoLongitud1,VehiculoLongitud2,VehiculoLongitud3,Ocupacion,Velocidad from MEDELLIN_HIST..H_TRAF_DETECTOR_DATA_1MIN where Logic_code in (select ELEM_GEN_COD_LOG  from MEDELLIN_CONF..TUN_ELEMENTO_GENERICO where TIP_EQUIP_CODIGO in (3, 14, 15) " +
                                              " and ELE_TIP_EQUIP_CODIGO in (14, null)) and fecha between '" + Ahora1String + "' and '" + AhoraString + "' order by fecha desc ", Conexion);
             DataTable dt = new DataTable();
             da.Fill(dt);
-
             if (dt.Rows.Count == 0)
             {
                 FileStream ArchivoTxT = new FileStream("C:/Traza/DAI.txt", FileMode.Append, FileAccess.Write);
@@ -107,15 +107,14 @@ namespace DatosTrafico
                 Escribir.WriteLine();
                 Escribir.Flush();
                 Escribir.Close();
-                string Mensaje = "No se encontró datos de CCTV para las "+DateTime.Now;
+                string Mensaje = "No se encontró datos de CCTV a esta hora ->";
                 Mail(Mensaje);
-                this.lblCCTV.Text = "Stoped";
+                this.lblCCTV.Text = "Stopped";
                 this.lblCCTV.BackColor= Color.Red ;
-
+                Conexion.Close();
             }
             else
-            {
-           
+            {        
                 FileStream ArchivoTxT = new FileStream("C:/Traza/DAI.txt", FileMode.Append, FileAccess.Write);
                 StreamWriter Escribir = new StreamWriter(ArchivoTxT);
                 Escribir.Write(DateTime.Now + " INFO Se encontró datos");
@@ -123,21 +122,142 @@ namespace DatosTrafico
                 Escribir.Flush();
                 Escribir.Close();
                 this.lblCCTV.Text = "Running";
+                Conexion.Close();
                 return;
             }    
-            Conexion.Close();
+           
         }
-
         private void BtnStart_Click(object sender, EventArgs e)
         {
             timer1.Enabled = true;
             timer1.Interval = 1000;
-            timer1.Start(); 
+            timer1.Start();
+            this.btnStart.Enabled = false;
         }
-
         private void Timer1_Tick(object sender, EventArgs e)
         {
             CCTV();
+            ARS();
+            FDT();
+        }
+        public void ARS()
+        {
+            Conexion.Open();
+            DateTime Ahora = DateTime.Now;
+            string AhoraString = Ahora.ToString("yyyy-MM-dd HH:mm:ss.FFF");
+            DateTime Ahora1 = Ahora.AddMinutes(-30);
+            string Ahora1String = Ahora1.ToString("yyyy-MM-dd HH:mm:ss.FFF");
+            SqlDataAdapter da = new SqlDataAdapter("select top 10 Logic_code,Fecha,Intensidad,VehiculoLongitud1,VehiculoLongitud2,VehiculoLongitud3,Ocupacion,Velocidad from MEDELLIN_HIST..H_TRAF_DETECTOR_DATA_1MIN where Logic_code in (select ELEM_GEN_COD_LOG " +
+                                "from MEDELLIN_CONF..TUN_ELEMENTO_GENERICO where TIP_EQUIP_CODIGO = 3 and ELE_TIP_EQUIP_CODIGO is null) and fecha between '" + Ahora1String + "' and '" + AhoraString + "' order by fecha desc ", Conexion);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count == 0)
+            {
+                FileStream ArchivoTxT = new FileStream("C:/Traza/ARS.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                Escribir.Write(DateTime.Now + " ERROR No se encontró datos ARS");
+                Escribir.WriteLine();
+                Escribir.Flush();
+                Escribir.Close();
+                this.label5.BackColor = Color.Red;
+                this.label5.Text = "Stopped";
+                string Mensaje = "No se encontró datos de ARS a esta hora -> ";
+                Mail(Mensaje);
+                Conexion.Close();
+            }
+            else
+            {
+                FileStream ArchivoTxT = new FileStream("C:/Traza/ARS.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                Escribir.Write(DateTime.Now + " INFO Se encontró datos de ARS");
+                Escribir.WriteLine();
+                Escribir.Flush();
+                Escribir.Close();
+                this.label5.Text = "Running";
+                Conexion.Close();
+                return;
+            }
+        }
+        public void FDT()
+        {
+            Conexion.Open();
+            DateTime Ahora = DateTime.Now;
+            DateTime Ahora2 = Ahora.AddMinutes(-90);
+            string AhoraString = Ahora2.ToString("yyyy-MM-dd HH:mm:ss.FFF");
+            DateTime Ahora1 = Ahora.AddMinutes(-180);
+            string Ahora1String = Ahora1.ToString("yyyy-MM-dd HH:mm:ss.FFF");
+        
+                SqlDataAdapter da = new SqlDataAdapter("select top 10 Logic_code,Fecha,Intensidad,VehiculoLongitud1,VehiculoLongitud2,VehiculoLongitud3,Ocupacion,Velocidad from MEDELLIN_HIST..H_TRAF_DETECTOR_DATA_HORA where Logic_code in (select ELEM_GEN_COD_LOG " +
+                                "from MEDELLIN_CONF..TUN_ELEMENTO_GENERICO where ELE_TIP_EQUIP_CODIGO = 1004) and fecha between '" + Ahora1String + "' and '" + AhoraString + "' ", Conexion);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+            if (dt.Rows.Count == 0)
+            {
+                FileStream ArchivoTxT = new FileStream("C:/Traza/FDT.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                Escribir.Write(DateTime.Now + " ERROR no encontrarón datos de FDT");
+                Escribir.WriteLine();
+                Escribir.Flush();
+                Escribir.Close();
+                this.label6.BackColor = Color.Red;
+                this.label6.Text = "Stopped";
+                string Mensaje = "No se encontró datos de FDT a esta hora -> ";
+                Mail(Mensaje);
+                Conexion.Close();
+            }
+        
+            else
+            {
+                FileStream ArchivoTxT = new FileStream("C:/Traza/FDT.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                Escribir.Write(DateTime.Now + " INFO Se encontrarón datos de FDT");
+                Escribir.WriteLine();
+                Escribir.Flush();
+                Escribir.Close();
+                this.label6.Text = "Running";
+                return;
+            }
+        }
+        public void Logger()
+        {
+            string Pro = "Logger-final";
+            Process[] Logger = Process.GetProcessesByName(Pro);
+            if (Logger.Length == 1)
+            {
+
+                FileStream ArchivoTxT = new FileStream("D:Diagnostico/Trazas/Procesos/Logger.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                Escribir.Write(DateTime.Now + ",El proceso " + Pro + ",esta corriendo");
+                Escribir.WriteLine();
+                Escribir.Flush();
+                Escribir.Close();
+            }
+            else
+            {
+                if (Logger.Length == 0)
+                {
+                 
+                    FileStream ArchivoTxT = new FileStream("D:Diagnostico/Trazas/Procesos/Logger.txt", FileMode.Append, FileAccess.Write);
+                    StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                    Escribir.Write(DateTime.Now + ",El proceso " + Pro + ",esta detenido");
+                    Escribir.WriteLine();
+                    Escribir.Flush();
+                    Escribir.Close();
+                }
+                if (Logger.Length > 1)
+                {
+                   
+                    FileStream ArchivoTxT = new FileStream("D:Diagnostico/Trazas/Procesos/Logger.txt", FileMode.Append, FileAccess.Write);
+                    StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                    Escribir.Write(DateTime.Now + ",El proceso " + Pro + ",esta más de una vez");
+                    Escribir.WriteLine();
+                    Escribir.Flush();
+                    Escribir.Close();
+                }
+                return;
+
+            }
         }
     }
 
