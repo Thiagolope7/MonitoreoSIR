acd72aec53adb352c82ab554e7ae0eeb8c0552b1
SANTIAGO AGUDELO LOPERA <santiagoagudelo152188@correo.itm.edu.co>
2019-04-01 15:51:50 -0500

    Operación 10%

--- DatosTrafico/Operacion.Designer.cs
+++ DatosTrafico/Operacion.Designer.cs
@@ -28,20 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(618, 48);
+            this.panel2.TabIndex = 18;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(3, 23);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(598, 18);
+            this.label15.TabIndex = 1;
+            this.label15.Text = "- En Bdv se encuentran las encargadas de escribir en SQL, verificamos su funciona" +
+    "miento.";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(3, 4);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(538, 18);
+            this.label14.TabIndex = 0;
+            this.label14.Text = "- En, SQL verificamos cada minuto que se encuentre datos en la tabla de 1 minuto." +
+    "";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCerrar.FlatAppearance.BorderSize = 10;
+            this.btnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCerrar.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCerrar.Location = new System.Drawing.Point(278, 378);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 47;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.BackColor = System.Drawing.Color.DarkGray;
+            this.label21.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label21.Location = new System.Drawing.Point(194, 278);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(18, 19);
+            this.label21.TabIndex = 46;
+            this.label21.Text = "0";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.BackColor = System.Drawing.Color.DarkGray;
+            this.label20.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label20.Location = new System.Drawing.Point(29, 278);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(170, 19);
+            this.label20.TabIndex = 45;
+            this.label20.Text = "Próxima ejecución en: ";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(284, 51);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(39, 19);
+            this.label17.TabIndex = 42;
+            this.label17.Text = "BDV";
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnStart.FlatAppearance.BorderSize = 10;
+            this.btnStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStart.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStart.Location = new System.Drawing.Point(79, 325);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 31;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.LimeGreen;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(150, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 19);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.LimeGreen;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(510, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 19);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LimeGreen;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(151, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 19);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkGray;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(396, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Logistica";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkGray;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(37, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 19);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "FDT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGray;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(37, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 19);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Kernel";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnUpdate.FlatAppearance.BorderSize = 10;
+            this.btnUpdate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnUpdate.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnUpdate.Location = new System.Drawing.Point(466, 140);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 49;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
             // 
             // Operacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
             this.ClientSize = new System.Drawing.Size(618, 403);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Operacion";
             this.Text = "Operacion";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
--- DatosTrafico/Operacion.cs
+++ DatosTrafico/Operacion.cs
@@ -2,19 +2,243 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using System.Collections;
+using System.Data.SqlClient;
 
 namespace DatosTrafico
 {
     public partial class Operacion : Form
     {
+        public static int CountVAL = 0;
+        public static int counLeer = 0;
+        SqlConnection cn;
         public Operacion()
         {
             InitializeComponent();
         }
+
+        static void Mail(string Mensaje)
+        {
+            FileStream ArchivoTxT = new FileStream("C:/Traza/MAIL.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+            DateTime TimeMail = DateTime.Now;
+            string TimeMail1 = TimeMail.ToString("yyyy-MM-dd HH:mm");
+            var fromAddress = new MailAddress("diagindra@gmail.com", "Rutina Hermes");
+            var toAddress = new MailAddress("santiagolopera13@gmail.com", "Santiago Agudelo");
+            const string fromPassword = "Medellin2017a!";
+            const string subject = "Alerta | Datos tráfico";
+            string body = Mensaje + TimeMail1;
+            MailAddress copyD = new MailAddress("davidmartinez.189@gmail.com", "David Martinez");
+            MailAddress copyE = new MailAddress("elmer.aua@gmail.com", "Elmer Usuga");
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+                try
+                {
+                    message.CC.Add(copyD);
+                    message.CC.Add(copyE);
+                    smtp.Send(message);
+                    Escribir.Write(DateTime.Now + " Se envió mail correctamente con el mensaje -> " + Mensaje + TimeMail1);
+                    Escribir.WriteLine();
+                    Escribir.Flush();
+                    Escribir.Close();
+                }
+                catch (Exception ex)
+                {
+                    Escribir.Write(DateTime.Now + " Lo siento en algún lado me perdí este debe ser el error:" + ex);
+                    Escribir.WriteLine();
+                    Escribir.Flush();
+                    Escribir.Close();
+                }
+        }
+        
+        public void Kernel()
+        {
+            FileStream ArchivoTxT = new FileStream("C:/Traza/Kernel.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+            string Proceso = "Kernel-final";
+            Process[] Kernel = Process.GetProcessesByName(Proceso);
+            if (Kernel.Length == 1)
+            {
+                Escribir.Write(DateTime.Now + ",El proceso " + Proceso + " esta corriendo");
+                Escribir.WriteLine();
+                Escribir.Flush();
+                Escribir.Close();
+                this.label4.Visible = true;
+                this.label4.Text = "Running";
+                this.label4.BackColor = Color.Green;
+            }
+            else
+            {
+                if (Kernel.Length == 0)
+                {
+                    Escribir.Write(DateTime.Now + ",El proceso " + Proceso + " esta detenido");
+                    Escribir.WriteLine();
+                    Escribir.Flush();
+                    Escribir.Close();
+                    string Mensaje = "La BDV Kernel se encuentra detenida a esta hora -> ";
+                    Mail(Mensaje);
+                    this.label4.Visible = true;
+                    this.label4.Text = "Stopped";
+                    this.label4.BackColor = Color.Red;
+                }
+                if (Kernel.Length > 1)
+                {
+                    Escribir.Write(DateTime.Now + ",El proceso " + Proceso + " esta más de una vez");
+                    Escribir.WriteLine();
+                    Escribir.Flush();
+                    Escribir.Close();
+                    string Mensaje = "La BDV Kernel se encuentra duplicada a esta hora -> ";
+                    Mail(Mensaje);
+                    this.label4.Visible = true;
+                    this.label4.Text = "Duplicated";
+                    this.label4.BackColor = Color.Yellow;
+                }
+                return;
+            }
+        }
+        public void Bdv_Logistica()
+        {
+            string path = "C:/EstadosLOG/Logistica/PRUEBA.txt";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }  
+            using (ManagementObjectSearcher mos = new ManagementObjectSearcher(
+             "SELECT CommandLine FROM Win32_Process WHERE name = 'Bdv_ES_CProg-Final.exe' "))
+            {
+                foreach (ManagementObject mo in mos.Get())
+                {
+                    FileStream ArchivoTxT = new FileStream("C:/Traza/ES_CPROG.txt", FileMode.Append, FileAccess.Write);
+                    StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                    Escribir.Write(mo["CommandLine"]);
+                    Escribir.WriteLine();
+                    Escribir.Flush();
+                    Escribir.Close();
+                    Console.WriteLine(mo["CommandLine"]);
+                }
+            }
+            StreamReader objReader = new StreamReader("C:/Traza/ES_CPROG.txt");
+            string sLine;
+            ArrayList arrLOG = new ArrayList();
+            while (!objReader.EndOfStream)
+            {
+                sLine = objReader.ReadLine();
+                if (!string.IsNullOrWhiteSpace(sLine))
+                    arrLOG.Add(sLine);
+            }
+            for (int i = 0; i < arrLOG.Count; i++)
+            {
+
+                string a = @"C:\AreaTrafico\Proyectos\Medellin\bin\Bdv_ES_CProg-Final.exe  SIGA\COLOMBIA\BDV_ES_LOGIST";
+                string b = arrLOG[i].ToString();
+                if (a == b)
+                {
+                    CountVAL = CountVAL + 1;
+                    //Escriba.Write(arrLOG[i].ToString());
+                    //Escriba.WriteLine();
+                    break;
+
+                }
+            }
+           
+            objReader.Close();
+            if (CountVAL != 0)
+            {
+                this.label5.Visible = true;
+                this.label5.Text = "Running";
+                this.label5.BackColor = Color.Green;
+             
+                FileStream ArchivoTxT = new FileStream("C:/Traza/Logistica.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter Escribir = new StreamWriter(ArchivoTxT);
+                Escribir.Write(DateTime.Now + " Bdv Logística está bien");
+                Escribir.WriteLine();
+                Escribir.Flush();
+                Escribir.Close();
+                CountVAL = 0;
+                return;
+            }
+            else
+            {
+                this.label5.Visible = true;
+                this.label5.Text = "Stoped";
+                this.label5.BackColor = Color.Red;
+                FileStream ArchivoTxT = new FileStream("C:/Traza/Logistica.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter Escr = new StreamWriter(ArchivoTxT);
+                Escr.Write(DateTime.Now + " Bdv Logística tiene problemas");
+                Escr.WriteLine();
+                Escr.Flush();
+                Escr.Close();
+                //timer1.Stop();
+                try
+                {
+                    FileStream Auto = new FileStream("C:/EstadosLOG/Bdv_Logistica.txt", FileMode.Append, FileAccess.Write);
+                    StreamWriter auto = new StreamWriter(Auto);
+                    DateTime Ahora = DateTime.Now;
+                    string AhoraString = Ahora.ToString("yyyyMMddHHmmss");
+                    string de = @"C:/EstadosLOG/Automatico/El_GEN+Val_Par.txt";
+                    string para = @"C:/EstadosLOG/Archivo/" + AhoraString + "_El_GEN+Val_Par.txt";
+                    if (File.Exists(de))
+                    {
+                        File.Move(de, para);
+                        auto.Write(DateTime.Now + " Se ha movido un backup automatico a " + para);
+                        auto.WriteLine();
+                        auto.Flush();
+                    }
+                    auto.Close();
+                    cn.Open();
+                    SqlCommand comando = new SqlCommand("select tvpee.EL_GEN_EXT_CODIGO, tvpee.VAL_PARAMETRO_ENT from TUN_ELEMENTO_GENERICO as teg join TUN_ELEMENTO_GENERICO_EXTERNO as tege on teg.ELEM_GEN_CODIGO = tege.ELEM_GEN_CODIGO and teg.TIP_EQUIP_CODIGO = tege.TIP_EQUIP_CODIGO join TUN_VALOR_PARAM_EQ_EXTERNO as tvpee on tvpee.EL_GEN_EXT_CODIGO = tege.EL_GEN_EXT_CODIGO and teg.TIP_EQUIP_CODIGO = tvpee.TIP_EQUIP_CODIGO and tvpee.PARAM_CODIGO = 14 where teg.TIP_EQUIP_CODIGO = 1009 and teg.ELEM_GEN_ACTIVO = 1 order by teg.ELEM_GEN_CODIGO", cn);
+                    SqlDataReader leer;
+                    leer = comando.ExecuteReader();
+
+                    FileStream Ensayo = new FileStream("C:/EstadosLOG/Automatico/El_GEN+Val_Par.txt", FileMode.Append, FileAccess.Write);
+                    StreamWriter Ensayar = new StreamWriter(Ensayo);
+                    while (leer.Read() == true)
+                    {
+
+                        Ensayar.Write(leer[0].ToString() + ";");
+                        Ensayar.Write(leer[1].ToString());
+                        Ensayar.WriteLine();
+                        Ensayar.Flush();
+                        counLeer = counLeer + 1;
+                    }
+                    Ensayar.Close();
+                    MessageBox.Show("Se ha guardado estado actual de los agentes", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //this.label8.Text = counLeer.ToString();
+                    cn.Close();
+                    this.btnUpdate.Enabled = true;
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+        }
+
+
     }
 }
+
